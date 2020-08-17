d54f00192e3ea67e59292e39409db1889633c8c3
Olgu SIRMAN <olgusirman@gmail.com>
2020-08-17 14:06:54 +0100

    Initial Commit

---
+++ TaskManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManager
+//
+//  Created by Olgu on 17.08.2020.
+//  Copyright © 2020 cloudadvise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
