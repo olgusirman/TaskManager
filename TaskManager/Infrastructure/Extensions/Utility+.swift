b22d0937dd83d2eb6727ce6db5f1029b93dc08eb
Olgu SIRMAN <olgusirman@gmail.com>
2020-10-03 14:10:25 +0100

    blur view extension added

--- TaskManager/Infrastructure/Extensions/Utility+.swift
+++ TaskManager/Infrastructure/Extensions/Utility+.swift
@@ -39,3 +39,15 @@ extension View {
                                         to: nil, from: nil, for: nil)
     }
 }
+
+struct Blur: UIViewRepresentable {
+    let style: UIBlurEffect.Style = .systemMaterial
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
